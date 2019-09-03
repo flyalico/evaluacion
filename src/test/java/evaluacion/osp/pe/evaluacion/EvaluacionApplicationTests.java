51b3c6ee6469f059559a538ab87c9a519499d0cc
Franko Yalico <flyalico@gmail.com>
2019-09-03 11:11:17 -0500

    avance

--- src/test/java/evaluacion/osp/pe/evaluacion/EvaluacionApplicationTests.java
+++ src/test/java/evaluacion/osp/pe/evaluacion/EvaluacionApplicationTests.java
@@ -1,16 +1,38 @@
 package evaluacion.osp.pe.evaluacion;
 
+import org.hamcrest.Matchers;
 import org.junit.Test;
+import org.junit.Before;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.test.web.servlet.MockMvc;
+
+import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class EvaluacionApplicationTests {
 
+    @Autowired
+    private WebApplicationContext wac;
+
+    private MockMvc mvc;
+
+    @Before
+    public void setup(){
+        this.mvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    }
+
     @Test
-    public void contextLoads() {
+    public void contextLoads() throws Exception{
+        this.mvc.perform(MockMvcRequestBuilders.get("/api/peliculas"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.size()", Matchers.equalTo(6)));
     }
 
 }
